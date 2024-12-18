07438c8ce19dc415cb8f75caa8f77e7990e4b0b1
kerem37mert <kerem37mert@hotmail.com>
2024-12-18 11:55:57 +0300

    Procedure added

---
+++ procedure.asm
@@ -0,0 +1,36 @@
+.model small
+.stack 64 
+.data 
+    message  db "Hello World!", 0ah, 0dh, 24h
+    message2 db "Hello Assembly!", 0ah, 0dh, 24h
+.code 
+    main proc far
+        mov ax, @data
+        mov ds, ax 
+        
+        call changeColor
+        
+        lea dx, message
+        call write     
+        
+        lea dx, message2
+        call write
+        
+        int 20h    
+    main endp
+                                                          
+    write proc near   
+        mov ah, 09h  
+        int 21h
+        ret
+    write endp
+    
+    changeColor proc near
+        mov ax, 0600
+        mov bh, 2fh
+        mov cx, 0000h
+        mov dx, 184fh
+        int 10h
+        ret    
+    changeColor endp
+end main            
